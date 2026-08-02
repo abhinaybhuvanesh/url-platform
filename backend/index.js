924ad8d5a5217f534ac35c8fc85151d19ffa4f8e
abhinay-78 <2310030078@klh.edu.in>
2026-08-02 19:41:31 +0530

    Fix Vercel backend

--- backend/index.js
+++ backend/index.js
@@ -129,5 +129,12 @@ app.get('/api/urls/my-links', protect, async (req, res, next) => {
 });
 
 app.use(errorHandler);
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+if (process.env.NODE_ENV !== 'production') {
+  const PORT = process.env.PORT || 3000;
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
+}
+
+module.exports = app;
